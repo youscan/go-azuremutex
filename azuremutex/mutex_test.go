a3021377e222b36bf1f383704f4a7c4403594dcc
Igor Tamashchuk <legigor@gmail.com>
2021-09-12 18:29:04 +0300

    Test concurrent increment

--- azuremutex/mutex_test.go
+++ azuremutex/mutex_test.go
@@ -2,9 +2,45 @@ package azuremutex
 
 import (
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 )
 
-func TestSomething(t *testing.T) {
-	assert.True(t, true, "Something wrong")
+func TestConcurrentIncrement(t *testing.T) {
+	const (
+		threads    = 5
+		operations = 10_000
+	)
+
+	var wg sync.WaitGroup
+	wg.Add(threads)
+
+	var count1 int
+	var count2 int
+
+	for i := 0; i < threads; i++ {
+		go func() {
+
+			options := MutexOptions{
+				ContainerName:      "locks",
+				UseStorageEmulator: true,
+			}
+			lock := NewLocker(options, "test")
+			err := lock.Lock()
+			assert.NoError(t, err)
+
+			for i := 0; i < operations; i++ {
+				count1++
+				count2 += 2
+			}
+
+			err = lock.Unlock()
+			assert.NoError(t, err)
+
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	assert.Equal(t, operations*threads, count1)
+	assert.Equal(t, operations*threads*2, count2)
 }
